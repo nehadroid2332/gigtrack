9a3a23d01bfb174a63b6038d96f7d1e4a1632cfa
Shivam Verma <er.verma.shivam@gmail.com>
2020-01-08 21:38:24 +0530

    changes

--- lib/ui/contactlist/contact_list_presenter.dart
+++ lib/ui/contactlist/contact_list_presenter.dart
@@ -9,93 +9,95 @@ class ContactListPresenter extends BasePresenter {
   ContactListPresenter(BaseContract view) : super(view);
 
   Stream<List<Contacts>> getContacts(String bandId, {String contactInit}) {
-    if (bandId != null && bandId.isNotEmpty)
-      return serverAPI.contactDB
-          .orderByChild('bandId')
-          .equalTo(bandId)
-          .onValue
-          .map((a) {
-        Map mp = a.snapshot.value;
-        if (mp == null) return null;
-        List<Contacts> acc = [];
-        for (var d in mp.values) {
-          final contact = Contacts.fromJSON(d);
+    // if (bandId != null && bandId.isNotEmpty)
+    //   return serverAPI.contactDB
+    //       .orderByChild('bandId')
+    //       .equalTo(bandId)
+    //       .onValue
+    //       .map((a) {
+    //     Map mp = a.snapshot.value;
+    //     if (mp == null) return null;
+    //     List<Contacts> acc = [];
+    //     for (var d in mp.values) {
+    //       final contact = Contacts.fromJSON(d);
 
-          // if (contactInit != null &&
-          //     getNameOrder(contact.name)[0].toLowerCase() ==
-          //         contactInit.toLowerCase()) {
-          //   acc.add(contact);
-          // } else if (contactInit.isEmpty) {
-          //   acc.add(contact);
-          // }
-          if (contactInit != null) {
-            if (contact.name.toLowerCase().contains(contactInit.toLowerCase()))
-              acc.add(contact);
-          } else
-            acc.add(contact);
-          // if (acc.length < 0) {
-          //   acc.add((contact));
-          // }
-        }
-        acc.sort((a, b) {
-          return a.name
-              .split(" ")
-              .last
-              .toLowerCase()
-              .compareTo(b.name.split(" ").last.toLowerCase());
-        });
-        return acc;
-      });
-    else
-      return serverAPI
-          .contactDB
-          .orderByChild('user_id')
-           .equalTo(serverAPI.currentUserId)
-          .onValue
-          .asyncMap((a) async {
-        Map mp = a.snapshot.value;
-        if (mp == null) return null;
-        List<Contacts> acc = [];
-        for (var d in mp.values) {
-          final contact = Contacts.fromJSON(d);
-          if (contact.bandId != null && contact.bandId.isNotEmpty) {
-            final res = await serverAPI.getBandDetails(contact.bandId);
-            if (res != null && res is Band) {
-              contact.band = res;
-            }
+    //       // if (contactInit != null &&
+    //       //     getNameOrder(contact.name)[0].toLowerCase() ==
+    //       //         contactInit.toLowerCase()) {
+    //       //   acc.add(contact);
+    //       // } else if (contactInit.isEmpty) {
+    //       //   acc.add(contact);
+    //       // }
+    //       if (contactInit != null) {
+    //         if (contact.name.toLowerCase().contains(contactInit.toLowerCase()))
+    //           acc.add(contact);
+    //       } else
+    //         acc.add(contact);
+    //       // if (acc.length < 0) {
+    //       //   acc.add((contact));
+    //       // }
+    //     }
+    //     acc.sort((a, b) {
+    //       return a.name
+    //           .split(" ")
+    //           .last
+    //           .toLowerCase()
+    //           .compareTo(b.name.split(" ").last.toLowerCase());
+    //     });
+    //     return acc;
+    //   });
+    // else
+    return serverAPI
+        .contactDB
+        // .orderByChild('user_id')
+        //  .equalTo(serverAPI.currentUserId)
+        .onValue
+        .asyncMap((a) async {
+      Map mp = a.snapshot.value;
+      if (mp == null) return null;
+      List<Contacts> acc = [];
+      for (var d in mp.values) {
+        final contact = Contacts.fromJSON(d);
+        if (contact.bandId != null && contact.bandId.isNotEmpty) {
+          final res = await serverAPI.getBandDetails(contact.bandId);
+          if (res != null && res is Band) {
+            contact.band = res;
           }
-          // if (contactInit != null &&
-          //     getNameOrder(contact.name)[0].toLowerCase() ==
-          //         contactInit.toLowerCase()) {
-          //   if (contact.user_id == serverAPI.currentUserId) {
-          //     acc.add(contact);
-          //   } else if (contact.user_id != null) {
-          //     final res = await serverAPI.getSingleUserById(contact.user_id);
-          //     if (res is User && (res.isUnder18Age ?? false)) {
-          //       if (res.guardianEmail == serverAPI.currentUserEmail) {
-          //         acc.add(contact);
-          //       }
-          //     }
-          //   }
-          // } else {
-          //   acc.add(contact);
-          // }
-
+        }
+        // if (contactInit != null &&
+        //     getNameOrder(contact.name)[0].toLowerCase() ==
+        //         contactInit.toLowerCase()) {
+        //   if (contact.user_id == serverAPI.currentUserId) {
+        //     acc.add(contact);
+        //   } else if (contact.user_id != null) {
+        //     final res = await serverAPI.getSingleUserById(contact.user_id);
+        //     if (res is User && (res.isUnder18Age ?? false)) {
+        //       if (res.guardianEmail == serverAPI.currentUserEmail) {
+        //         acc.add(contact);
+        //       }
+        //     }
+        //   }
+        // } else {
+        //   acc.add(contact);
+        // }
+        if (contact.user_id == serverAPI.currentUserId ||
+            contact.bandId == bandId) {
           if (contactInit != null) {
             if (contact.name.toLowerCase().contains(contactInit.toLowerCase()))
               acc.add(contact);
           } else
             acc.add(contact);
         }
-        acc.sort((a, b) {
-          return a.name
-              .split(" ")
-              .last
-              .toLowerCase()
-              .compareTo(b.name.split(" ").last.toLowerCase());
-        });
-        return acc;
+      }
+      acc.sort((a, b) {
+        return a.name
+            .split(" ")
+            .last
+            .toLowerCase()
+            .compareTo(b.name.split(" ").last.toLowerCase());
       });
+      return acc;
+    });
   }
 
   String getNameOrder(String name) {
