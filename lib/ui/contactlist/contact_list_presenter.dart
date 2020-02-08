523d1d97f96f4a64fcf429c4a0d15955e3c6714c
Shivam Verma <er.verma.shivam@gmail.com>
2020-02-08 13:44:11 +0530

    changes

--- lib/ui/contactlist/contact_list_presenter.dart
+++ lib/ui/contactlist/contact_list_presenter.dart
@@ -10,49 +10,7 @@ class ContactListPresenter extends BasePresenter {
   ContactListPresenter(BaseContract view) : super(view);
 
   Stream<List<Contacts>> getContacts(String bandId, {String contactInit}) {
-    // if (bandId != null && bandId.isNotEmpty)
-    //   return serverAPI.contactDB
-    //       .orderByChild('bandId')
-    //       .equalTo(bandId)
-    //       .onValue
-    //       .map((a) {
-    //     Map mp = a.snapshot.value;
-    //     if (mp == null) return null;
-    //     List<Contacts> acc = [];
-    //     for (var d in mp.values) {
-    //       final contact = Contacts.fromJSON(d);
-
-    //       // if (contactInit != null &&
-    //       //     getNameOrder(contact.name)[0].toLowerCase() ==
-    //       //         contactInit.toLowerCase()) {
-    //       //   acc.add(contact);
-    //       // } else if (contactInit.isEmpty) {
-    //       //   acc.add(contact);
-    //       // }
-    //       if (contactInit != null) {
-    //         if (contact.name.toLowerCase().contains(contactInit.toLowerCase()))
-    //           acc.add(contact);
-    //       } else
-    //         acc.add(contact);
-    //       // if (acc.length < 0) {
-    //       //   acc.add((contact));
-    //       // }
-    //     }
-    //     acc.sort((a, b) {
-    //       return a.name
-    //           .split(" ")
-    //           .last
-    //           .toLowerCase()
-    //           .compareTo(b.name.split(" ").last.toLowerCase());
-    //     });
-    //     return acc;
-    //   });
-    // else
-    return serverAPI.contactDB
-        .orderByChild('user_id')
-        //.equalTo(serverAPI.currentUserId)
-        .onValue
-        .asyncMap((a) async {
+    return serverAPI.contactDB.onValue.asyncMap((a) async {
       Map mp = a.snapshot.value;
       if (mp == null) return null;
       List<Contacts> acc = [];
@@ -64,37 +22,20 @@ class ContactListPresenter extends BasePresenter {
             contact.band = res;
           }
         }
-        // if (contactInit != null &&
-        //     getNameOrder(contact.name)[0].toLowerCase() ==
-        //         contactInit.toLowerCase()) {
-        //   if (contact.user_id == serverAPI.currentUserId) {
-        //     acc.add(contact);
-        //   } else if (contact.user_id != null) {
-        //     final res = await serverAPI.getSingleUserById(contact.user_id);
-        //     if (res is User && (res.isUnder18Age ?? false)) {
-        //       if (res.guardianEmail == serverAPI.currentUserEmail) {
-        //         acc.add(contact);
-        //       }
-        //     }
-        //   }
-        // } else {
-        //   acc.add(contact);
-        // }
+
         if (bandId.isEmpty) {
           if (contact.band != null) {
             if (contact.band.bandmates.keys
-                .contains(serverAPI.currentUserEmail.replaceAll(".", "")) ||
+                    .contains(serverAPI.currentUserEmail.replaceAll(".", "")) ||
                 contact.band.userId == serverAPI.currentUserId) {
               if (contactInit != null) {
                 if (contact.name
                     .toLowerCase()
                     .contains(contactInit.toLowerCase())) acc.add(contact);
-              } else{
+              } else {
                 acc.add(contact);
+              }
             }
-
-            }
-
           }
           if (contact.userId == serverAPI.currentUserId) {
             if (acc.contains(contact)) {
@@ -104,12 +45,11 @@ class ContactListPresenter extends BasePresenter {
                 if (contact.name
                     .toLowerCase()
                     .contains(contactInit.toLowerCase())) acc.add(contact);
-              } else{
+              } else {
                 acc.add(contact);
               }
             }
           }
-
         } else {
           if (contact.bandId == bandId) {
             if (contactInit != null) {
--- lib/ui/instrumentlist/instrument_list_presenter.dart
+++ lib/ui/instrumentlist/instrument_list_presenter.dart
@@ -10,51 +10,45 @@ class InstrumentListPresenter extends BasePresenter {
   InstrumentListPresenter(BaseContract view) : super(view);
 
   Stream<List<UserInstrument>> getList(String bandId) {
-    if (bandId != null && bandId.isNotEmpty)
-      return serverAPI.equipmentsDB
-          .orderByChild('bandId')
-          .equalTo(bandId)
-          .onValue
-          .map((a) {
-        Map mp = a.snapshot.value;
-        if (mp == null) return null;
-        List<UserInstrument> acc = [];
-        for (var d in mp.values) {
-          acc.add(UserInstrument.fromJSON(d));
+    return serverAPI
+        .equipmentsDB
+        //  .orderByChild('user_id')
+        //  .equalTo(serverAPI.currentUserId)
+        .onValue
+        .asyncMap((a) async {
+      Map mp = a.snapshot.value;
+      if (mp == null) return null;
+      List<UserInstrument> acc = [];
+      for (var d in mp.values) {
+        final contact = UserInstrument.fromJSON(d);
+        if (contact.bandId != null && contact.bandId.isNotEmpty) {
+          final res = await serverAPI.getBandDetails(contact.bandId);
+          if (res != null && res is Band) {
+            contact.band = res;
+          }
         }
-        acc.sort((a, b) {
-          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
-        });
-        return acc;
-      });
-    else
-      return serverAPI
-          .equipmentsDB
-          //  .orderByChild('user_id')
-          //  .equalTo(serverAPI.currentUserId)
-          .onValue
-          .asyncMap((a) async {
-        Map mp = a.snapshot.value;
-        if (mp == null) return null;
-        List<UserInstrument> acc = [];
-        for (var d in mp.values) {
-          final contact = UserInstrument.fromJSON(d);
-          if (contact.bandId != null && contact.bandId.isNotEmpty) {
-            final res = await serverAPI.getBandDetails(contact.bandId);
-            if (res != null && res is Band) {
-              contact.band = res;
+        if (contact.bandId.isEmpty) {
+          if (contact.user_id == serverAPI.currentUserId) {
+            if (acc.contains(contact)) {
+              continue;
+            } else
+              acc.add(contact);
+          }
+        } else {
+          if (contact.band != null && bandId.isNotEmpty) {
+            if ( // activites.band.bandmates.keys.contains(serverAPI.currentUserEmail.replaceAll(".", "")) &&
+                contact.band.id == bandId) {
+              acc.add(contact);
             }
           }
-          if (contact.user_id == serverAPI.currentUserId) {
-            acc.add(contact);
-          }
-          // acc.add(UserInstrument.fromJSON(d));
         }
-        acc.sort((a, b) {
-          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
-        });
-        return acc;
+        // acc.add(UserInstrument.fromJSON(d));
+      }
+      acc.sort((a, b) {
+        return a.name.toLowerCase().compareTo(b.name.toLowerCase());
       });
+      return acc;
+    });
   }
 
   void getBand(String bandId) async {
